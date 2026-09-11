449c9c8be1a9b80b51cce0e06a37fc85335e2859
Hanzerik307 <hanzerik.wyoming@gmail.com>
2026-09-10 20:03:37 -0600

    Updated for 1.8.0

--- build-cosmic.sh
+++ build-cosmic.sh
@@ -46,10 +46,10 @@ REPO_DIR="$HOME/local-repo"
 PATCH_DIR="${SCRIPT_DIR}/cosmic-patches"
 
 # Edit Version to match cosmic-epoch release
-COSMIC_VERSION="1.7.0"
+COSMIC_VERSION="1.8.0"
 BUILD_DATE=$(date +%Y%m%d)
 
-# Formats output like: 1.7.0+deb13-20260804 or 1.7.0+deb14-20260804
+# Formats output like: 1.8.0+deb13-20260804 or 1.8.0+deb14-20260804
 FULL_VERSION="${COSMIC_VERSION}+${DEB_TAG}-${BUILD_DATE}"
 
 # Log file output location
